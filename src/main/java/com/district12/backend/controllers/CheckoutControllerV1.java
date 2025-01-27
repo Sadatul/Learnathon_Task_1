b2266d973e9d404dddd519605a3116979f41102c
Ishrak-Adit07 <ishrak.adit07@gmail.com>
2025-01-27 19:13:55 +0600

    Exception handle commit: Proper responses for unsuccessful check out attempts

--- src/main/java/com/district12/backend/controllers/CheckoutControllerV1.java
+++ src/main/java/com/district12/backend/controllers/CheckoutControllerV1.java
@@ -11,7 +11,9 @@ import com.district12.backend.utils.SecurityUtils;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -29,24 +31,31 @@ public class CheckoutControllerV1 {
 
     // POST /checkout/items
     @PostMapping(path = "/items")
-    public ResponseEntity<OrderResponse> checkOutItemsForUser(
+    public ResponseEntity<Object> checkOutItemsForUser(
             @Valid @RequestBody CheckoutRequest checkoutRequest) {
 
-        cartItemService.doAllCartItemsBelongToUser(
-                checkoutRequest.getCartItemIds(), SecurityUtils.getOwnerID());
-        cartItemService.isAnyCartItemInAnotherOrder(checkoutRequest.getCartItemIds());
+        try {
+            cartItemService.doAllCartItemsBelongToUser(
+                    checkoutRequest.getCartItemIds(), SecurityUtils.getOwnerID());
+            cartItemService.isAnyCartItemInAnotherOrder(checkoutRequest.getCartItemIds());
 
-        User user = userService.getUserById(SecurityUtils.getOwnerID());
-        Order newOrder = orderService.createOrder(user, checkoutRequest.getPaymentMethod());
-        cartItemService.updateCartItemsOrderId(checkoutRequest.getCartItemIds(), newOrder);
+            User user = userService.getUserById(SecurityUtils.getOwnerID());
+            Order newOrder = orderService.createOrder(user, checkoutRequest.getPaymentMethod());
+            cartItemService.updateCartItemsOrderId(checkoutRequest.getCartItemIds(), newOrder);
 
-        OrderResponse orderResponse = new OrderResponse(
-                newOrder.getId(), SecurityUtils.getOwnerID(),
-                newOrder.getTimestamp(), newOrder.getStatus());
+            OrderResponse orderResponse = new OrderResponse(
+                    newOrder.getId(), SecurityUtils.getOwnerID(),
+                    newOrder.getTimestamp(), newOrder.getStatus());
 
-        URI savedCartItemUri = ServletUriComponentsBuilder.fromCurrentRequestUri()
-                .path("/{id}").buildAndExpand(orderResponse.getOrderId()).toUri();
-        return ResponseEntity.created(savedCartItemUri).body(orderResponse);
+            URI savedCartItemUri = ServletUriComponentsBuilder.fromCurrentRequestUri()
+                    .path("/{id}").buildAndExpand(orderResponse.getOrderId()).toUri();
+            return ResponseEntity.created(savedCartItemUri).body(orderResponse);
+
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).body(e.getMessage());
+        } catch (UsernameNotFoundException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+        }
 
     }
 
